4486ad7cfb7270fc4f63202e239f6c8da60bdd9f
inoahgarciaruiz <inoah.garciaruiz@gmail.com>
2018-02-03 18:57:59 +0100

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -27,36 +27,36 @@ divList = ["div", "divide", "/"]
 
 # Get the parameters.
 if (len(sys.argv) == 4):
-   op = sys.argv[1]
-   try:
-      op1 = float(sys.argv[2])
-      op2 = float(sys.argv[3])
-   except ValueError:
-      print("[ERROR: ] Operands must be numbers.")
-      exit()
+    op = sys.argv[1]
+    try:
+        op1 = float(sys.argv[2])
+        op2 = float(sys.argv[3])
+    except ValueError:
+        print("[ERROR: ] Operands must be numbers.")
+        exit()
 
 else:
-   print("[USAGE: ] python calculadora.py <operation> <operand1> <operand2>")
-   exit()
+    print("[USAGE: ] python calculadora.py <operation> <operand1> <operand2>")
+    exit()
 
 # Compute the operation.
 if op in sumList:
-   result = op1 + op2
-   opSym = "+"
+    result = op1 + op2
+    opSym = "+"
 elif op in subList:
-   result = op1 - op2
-   opSym = "-"
+    result = op1 - op2
+    opSym = "-"
 elif op in mulList:
-   result = op1 * op2
-   opSym = "*"
+    result = op1 * op2
+    opSym = "*"
 elif op in divList:
-   result = op1 / op2
-   opSym = "/"
+    result = op1 / op2
+    opSym = "/"
 else:
-   print("[ERROR: ] Operation not included.")
-   exit()
+    print("[ERROR: ] Operation not included.")
+    exit()
 
 # Display result.
-print('[RESULT: ] ' + str(op1) + ' ' + opSym + ' ' + str(op2) + ' = ' + str(result))
+print('[RESULT: ] ' + str(op1) + ' ' + opSym + ' ' + str(op2) +
+      ' = ' + str(result))
 exit()
-
